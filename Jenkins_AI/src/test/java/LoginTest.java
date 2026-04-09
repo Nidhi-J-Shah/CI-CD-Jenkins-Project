5f7a918b6020884a834218f0c5e83f9aea2d8f79
Nidhi Shah <nidhijshah195@gmail.com>
2026-04-09 13:27:04 +0530

    Added invalid and empty login test scenarios

--- Jenkins_AI/src/test/java/LoginTest.java
+++ Jenkins_AI/src/test/java/LoginTest.java
@@ -1,23 +1,59 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
-    @Test
-    public void validLogin() {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Documents\\chromedriver.exe");
-
-        WebDriver driver = new ChromeDriver();
-
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/login");
+    }
 
+    @Test
+    public void validLogin() {
         driver.findElement(By.id("username")).sendKeys("tomsmith");
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.cssSelector("button[type='submit']")).click();
 
-        driver.quit();
+        // Optional: Verify login success message
+        String message = driver.findElement(By.id("flash")).getText();
+        System.out.println("Valid login message: " + message);
+    }
+
+    @Test
+    public void invalidLogin() {
+        driver.findElement(By.id("username")).sendKeys("wronguser");
+        driver.findElement(By.id("password")).sendKeys("wrongpass");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        // Optional: Verify error message
+        String message = driver.findElement(By.id("flash")).getText();
+        System.out.println("Invalid login message: " + message);
+    }
+
+    @Test
+    public void emptyFieldsLogin() {
+        driver.findElement(By.id("username")).sendKeys("");
+        driver.findElement(By.id("password")).sendKeys("");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        // Optional: Verify error message
+        String message = driver.findElement(By.id("flash")).getText();
+        System.out.println("Empty fields login message: " + message);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
